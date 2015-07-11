d94f09b424e7519e541627b07887d62ea6c78882
Walter Gonzalez Domenzain <walter.gonzalez@smartplace.mx>
2015-07-11 09:43:37 -0500

    Initial Commit

---
+++ CM-Json/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  CM-Json
+//
+//  Created by Walter Gonzalez Domenzain on 11/07/15.
+//  Copyright (c) 2015 Smartplace. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ CM-Json/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CM-Json
+//
+//  Created by Walter Gonzalez Domenzain on 11/07/15.
+//  Copyright (c) 2015 Smartplace. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
